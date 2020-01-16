4aea054f4bc7b5d3b4fe5b6af8fc224a8f7568a7
e.markov@blubito.com <e.markov@blubito.com>
2020-01-16 06:24:12 +0000

    zend database seeder, must reload config cache and change db in .env

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+	public $timestamps = false;
+	
     protected $fillable = [
         'name', 'url', 'description', 'brand_id', 'category_id'
     ];
