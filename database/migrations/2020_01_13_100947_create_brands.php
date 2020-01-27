1524acf792d9c88d48dc9129bfa1e3a22cf4afc1
ddonchev <d.donchev@blubito.com>
2020-01-27 09:32:29 +0000

    added

---
+++ database/migrations/2020_01_13_100947_create_brands.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBrands extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('url');
+            $table->string('description');
+            $table->string('created_at_ip',45)->nullable();
+            $table->string('updated_at_ip',45)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
