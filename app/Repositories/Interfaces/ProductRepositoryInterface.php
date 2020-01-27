c3d391ccd43e06936a07452dcf0e2a5f5b7fef2a
gstateva <ganka.stateva@happyshops.ch>
2020-01-27 11:55:07 +0200

    create repository pattern

---
+++ app/Repositories/Interfaces/ProductRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Repositories\Interfaces;
+
+use App\Brand;
+use App\Category;
+
+interface ProductRepositoryInterface
+{
+    public function all();
+    public function getByCategory(Category $category);
+    public function getByBrand(Brand $brand);
+}
