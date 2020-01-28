1b078eb3569696183b174fc05a82e4d0716f71a8
gidulski <b.gidulski@blubito.com>
2020-01-28 17:17:26 +0200

    added loading spinner for products

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,34 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+<style>
+.loader {
+    position: fixed;
+    z-index: 10;
+    left: 50%;
+    top: 40%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    border: 16px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 16px solid #3498db;
+    width: 120px;
+    height: 120px;
+    -webkit-animation: spin 2s linear infinite; /* Safari */
+    animation: spin 2s linear infinite;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+    0% { -webkit-transform: rotate(0deg); }
+    100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+</style>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <div class="loader" style="display: none;"></div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -57,6 +58,12 @@
         $(document).ready(function(){
             
             filter($('.filter-products'));
+
+            $(document).ajaxStart(function() {
+                $(".loader").show();
+            }).ajaxStop(function() {
+                $(".loader").hide();
+            });
 
             $(document).on('click', '.pagination .page-link',function(event)
             {
