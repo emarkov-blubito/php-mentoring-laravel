8228e7b713b1189183230cc5ba56d23a9a276b0b
ddonchev <d.donchev@blubito.com>
2020-03-09 14:16:29 +0200

    reformatted dropdowns for categories and brands in products

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Auth::routes();
 Route::get('/', 'PagesController@homepage');
 Route::get('/load-products/{offset}', 'PagesController@loadProducts');
 Route::get('/products/filter', 'ProductController@filter');
-Route::get('/categories/{url}', 'CategoryController@getProductsByUrl');
 Route::get('/brands/{url}', 'BrandController@getProductsByUrl');
 Route::resource('categories', 'CategoryController');
+Route::get('/categories/{url}', 'CategoryController@getProductsByUrl');
 Route::resource('brands', 'BrandController');
 Route::resource('products', 'ProductController');
 
