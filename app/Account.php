d94d0568193cde25ca3d5985cd81b30d22d8cdf7
e.markov@blubito.com <e.markov@blubito.com>
2020-04-12 22:36:39 +0300

    api test

---
+++ app/Account.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'first_order', 
+        'rating_date', 
+        'last_order', 
+        'registration',
+        'last_modified', 
+        'address', 
+        'rating_description', 
+        'ebilling_mail',
+        'payment_terms', 
+        'ta', 
+        'country_code', 
+        'rating_indicator',
+        'mc_code',
+        'ebilling', 
+        'average_days', 
+        'phone', 
+        'customer_code',
+    ];
+
+    public static function seedData($json)
+    {
+        $data = json_decode($json);
+        if ($data) {
+            foreach ($data as $el) {
+                Account::create($el);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Account;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::put('/api_basic/entity-accounts', function (Request $request) {
+    Account::seedData($request->json);
 });
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
